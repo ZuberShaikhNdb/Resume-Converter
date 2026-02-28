06247b82da4dd3a69e837efd7d7152f2d672f7c4
ZuberShaikhNdb <zubershaikhaqeel@gmail.com>
2026-03-01 01:31:39 +0530

    added Template1 & Template2 with Apexon corporate layout

--- backend/app/template/template_service.py
+++ backend/app/template/template_service.py
@@ -3,17 +3,19 @@ from weasyprint import HTML
 import os
 
 
-def generate_resume(data, output_path="app/uploads/output.pdf"):
+def generate_resume(data, template_name="template1.html", output_path="app/uploads/output.pdf"):
 
     os.makedirs(os.path.dirname(output_path), exist_ok=True)
 
-    template_path = os.path.abspath("app/template")   # ⭐ ADD THIS
+    template_path = os.path.abspath("app/template")
 
     env = Environment(loader=FileSystemLoader(template_path))
-    template = env.get_template("apexon_template.html")
+
+    # 🔥 THIS LINE IS IMPORTANT
+    template = env.get_template(template_name)
 
     html_content = template.render(**data)
 
-    HTML(string=html_content, base_url=template_path).write_pdf(output_path)   # ⭐ ADD base_url
+    HTML(string=html_content, base_url=template_path).write_pdf(output_path)
 
     return output_path
